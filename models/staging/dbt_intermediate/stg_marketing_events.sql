c3c736115e61a7be9ff41c9fb6b6bc3b3aa50657
Abraham Setiawan <abraham.setiawan@rebtel.com>
2026-05-24 20:24:11 +0200

    add dbt intermediate models

---
+++ models/staging/dbt_intermediate/stg_marketing_events.sql
@@ -0,0 +1,28 @@
+{{
+  config(
+    materialized = 'incremental',
+    unique_key = 'event_id',
+    )
+}}
+
+with seed_marketing_events as (
+    select * from {{ ref('seed_marketing_events') }}
+    where true
+        and event_date <= '2026-05-16'  -- only load up to this date for demo purposes
+    {% if is_incremental() %}
+        and event_date >= (select max(event_date) from {{ this }})
+    {% endif %}
+),
+
+final as (
+    select
+       event_id,
+       event_timestamp,
+       event_type,
+       customer_id,
+       campaign_name,
+       page_url
+    from seed_marketing_events
+)
+
+select * from final
